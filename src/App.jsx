cc944a5c660e3626ff4c926786f1e94ea6754cdb
Kunal-Paste <pastekunal6@gmail.com>
2026-04-06 16:02:30 +0530

    fix: remove unnecessary comment in photo fetching function

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className='h-screen w-full bg-gray-700 p-7'>
       <button className='p-3 bg-emerald-700 rounded-3xl font-bold' onClick={async ()=>{
-        const data = await getPhotos('cat');
+        const data = await getPhotos('cat');//
         console.log(data.results);
       }}>get photos</button>
     </div>
