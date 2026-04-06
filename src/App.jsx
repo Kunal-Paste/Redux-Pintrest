15d08ba0156f7572042786d898387026d10e13a2
Kunal-Paste <pastekunal6@gmail.com>
2026-04-06 16:00:22 +0530

    feat: add axios for API calls and implement photo fetching functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { getPhotos } from './api/mediaApi'
 
 const App = () => {
   return (
-    <div className='h-screen w-full bg-gray-700'>App</div>
+    <div className='h-screen w-full bg-gray-700 p-7'>
+      <button className='p-3 bg-emerald-700 rounded-3xl font-bold' onClick={async ()=>{
+        const data = await getPhotos('cat');
+        console.log(data.results);
+      }}>get photos</button>
+    </div>
   )
 }
 
